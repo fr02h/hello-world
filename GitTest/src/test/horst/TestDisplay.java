9749f99be08b3a465cb382e7c0146d33e6cac2cb
Horst <hf@cataneo.de>
2014-02-22 18:28:44 +0100

    Schleife über Data

--- GitTest/src/test/horst/TestDisplay.java
+++ GitTest/src/test/horst/TestDisplay.java
@@ -5,5 +5,11 @@ public class TestDisplay {
 	public void display()
 	{
 		System.out.println("TestDisplay");
+		
+		int[] data = { -1,-2,-3,-4,-5};
+		
+		for (int i : data) {
+			System.out.println("Test : " +i);
+		}
 	}
 }
