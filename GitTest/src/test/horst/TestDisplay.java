7d61e1c854b32f7d8b3092ae3f7b7c33794463ca
Horst <hf@cataneo.de>
2014-02-25 13:29:39 +0100

    Fehler behoben

--- GitTest/src/test/horst/TestDisplay.java
+++ GitTest/src/test/horst/TestDisplay.java
@@ -8,16 +8,13 @@ public class TestDisplay {
 		
 		int[] data = { -1,-2,-3,-4,-5,1,2,3,4,5};
 
-		int[] data = { -1,-2,-3,-4,-5};
-
-		
 		for (int i : data) {
 			System.out.println("Test : " +i);
 		}
 
 		for (int y = 0; y < 10; y++) {
 			System.out.println("i: " + y);
-			
+		}	
 
 		
 		for (int z = 0; z < 10; z++) {
