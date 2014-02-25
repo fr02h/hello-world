3b230039e361e5ce33990188f0ca87df28f628ad
Horst <hf@cataneo.de>
2014-02-25 13:45:57 +0100

    Release 1 kommentar

--- GitTest/src/test/horst/TestDisplay.java
+++ GitTest/src/test/horst/TestDisplay.java
@@ -23,7 +23,7 @@ public class TestDisplay {
 			System.out.println("------------------------------");
 			System.out.println("--------------XXX----------------");
 		}
-		
+		System.out.println("Release 1");
 		System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
 	}
 }
